30a04d3517a9982d6ddaf48dad6315a73f39150b
Ovadya33 <timurgts2710@gmail.com>
2023-03-24 14:02:16 +0300

    CW 42 UDP

--- src/telran/net/TcpServerClient.java
+++ src/telran/net/TcpServerClient.java
@@ -1,78 +1,33 @@
 package telran.net;
 import java.io.*;
 import java.net.*;
-
 public class TcpServerClient implements Runnable{
 private Socket socket;
 private ObjectInputStream input;
 private ObjectOutputStream output;
 private Protocol protocol;
-
 public TcpServerClient(Socket socket, Protocol protocol) throws IOException {
 	this.protocol = protocol;
 	this.socket = socket;
-	input = new ObjectInputStream(socket.getInputStream());	
+	input = new ObjectInputStream(socket.getInputStream());
 	output = new ObjectOutputStream(socket.getOutputStream());
+	
 }
 	@Override
 	public void run() {
-		try {
-			while(true) {
+		boolean running = true;
+		while(running) {
+			try {
 				Request request = (Request) input.readObject();
 				Response response = protocol.getResponse(request);
 				output.writeObject(response);
+			} catch (EOFException e) {
+				System.out.println("client closed connection");
+				running = false;
+			} catch (Exception e)  {
+				throw new RuntimeException(e.toString());
+				
 			}
-		} catch (EOFException e) {
-			System.out.println("client closed connection");
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e.toString());
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 		}
 		
 	}
